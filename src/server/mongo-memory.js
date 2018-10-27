a9ff6ca137aeab2e851fbd53fd2545a38a3e4cd6
Mattias Ahlsen <mattias.ahlsen@gmail.com>
2018-10-27 21:38:00 +0200

    Small fix to mongo-memory, now printing uri of mongo server and choosing same uri every time.

--- src/server/mongo-memory.js
+++ src/server/mongo-memory.js
@@ -1,11 +1,20 @@
 import MongoMemoryServer from 'mongodb-memory-server'
+import { debug } from './debug'
 
 async function createDb () {
-  const mongod = new MongoMemoryServer()
+  const mongod = new MongoMemoryServer({
+    instance: {
+      ip: '127.0.0.1',
+      dbName: 'comments-mongo-memory-server',
+      port: 27018 // standard mongo port + 1
+    }
+  })
   const uri = await mongod.getConnectionString()
   const port = await mongod.getPort()
   const dbPath = await mongod.getDbPath()
   const dbName = await mongod.getDbName()
+
+  debug('Mongo memory server uri: ' + uri)
 
   return {
     mongod,
