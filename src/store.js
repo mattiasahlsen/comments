ba1cf5bacb974a411ca941a75e68cd6c7b51c3be
Mattias Ahlsen <mattias.ahlsen@gmail.com>
2018-12-26 17:43:45 +0100

    Error handling added to store. Displayed in App.vue, so globally.

--- src/store.js
+++ src/store.js
@@ -7,10 +7,33 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    error: null
   },
   mutations: {
-
+    error(state, msg) {
+      state.error = msg
+    },
+    clearError(state) {
+      state.error = null
+    },
+    status(state, code) {
+      switch (code) {
+        case 401:
+          this.error = 'Authentication failed'
+          return
+        case 404:
+          this.error = 'Invalid comment section url'
+          return
+        case 422:
+          this.error = 'Invalid fields.'
+          return
+        case 500:
+          this.error = 'Error on the server.'
+          return
+        default:
+          this.error = 'HTTP error ' + code
+      }
+    }
   },
   actions: {
 
