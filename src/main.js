08f7f99d42fd4ffcec953d6c397ee7c1d8ec4e9d
Mattias Ahlsen <mattias.ahlsen@gmail.com>
2019-02-23 14:05:13 +0100

    Added cookie consent using npm module cookieconsent.

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'cookieconsent/build/cookieconsent.min.js'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faThumbsUp, faThumbsDown, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import 'modern-normalize/modern-normalize.css'
+import 'cookieconsent/build/cookieconsent.min.css'
 
 library.add(faThumbsUp)
 library.add(faThumbsDown)
@@ -26,6 +28,17 @@ if (localStorage.getItem('loggedIn') === 'true') {
       localStorage.removeItem('loggedIn')
     })
 }
+
+window.cookieconsent.initialise({
+  'palette': {
+    'popup': {
+      'background': '#000'
+    },
+    'button': {
+      'background': '#f1d600'
+    }
+  }
+})
 
 new Vue({
   router,
