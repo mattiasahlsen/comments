17de55fcf76f805ccc8adbcf1374a2948b511dfa
Mattias Ahlsen <mattias.ahlsen@gmail.com>
2019-03-07 19:29:42 +0100

    From 200 to 300 font weight on linkbutton text to make it more visible on mobile. When registering and logging in, make username lowercase.

--- src/App.vue
+++ src/App.vue
@@ -24,15 +24,6 @@
       </div>
     </footer>
 
-    <!--<div :class="{ dark: error }">
-    </div>
-
-    <div v-if="error" class="error-alert">
-      <div>
-        <p>{{error}}</p>
-      </div>
-    </div>-->
-
   </div>
 </template>
 
@@ -70,7 +61,7 @@ export default {
 </script>
 <style lang="scss">
 
-@import url('https://fonts.googleapis.com/css?family=Raleway:100,400,700');
+@import url('https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,700');
 
 #app {
   width: 100%;
